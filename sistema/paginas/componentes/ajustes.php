7ac91f6dde0cc758868637b450dab0a139abb0cc
Juan Diego Fernandez <fernandezgonzalezjuandiego95@gmail.com>
2023-04-22 14:21:32 -0500

    Trabajando en la ventana modal

--- sistema/paginas/componentes/ajustes.php
+++ sistema/paginas/componentes/ajustes.php
@@ -29,7 +29,9 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
 <div class="div_content_text">
   <h2 class="text_cont">Cambiar Contraseña:</h2>
 </div>
-
+<button type="button" class="btn btn-default" data-toggle="modal" data-target="#modal-default">
+                  Launch Default Modal
+                </button>
 <form action=""  method="post" class="form-ajustes">
   <input class="input-ajustes" type="password" id="nueva" name="nueva"  placeholder="Ingrese su nueva contraseña" required/>
   <br>
@@ -42,3 +44,29 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
   <img src="../assets/img/monster_artist.svg" alt="Ilustracion" class="img1-ajustes">
   <img src="../assets/img/updates.svg" alt="Ilustracion" class="img2-ajustes">
 </div>
+
+<div class="modal fade" id="modal-default">
+  <div class="modal-dialog">
+    <div class="modal-content">
+          <div class="modal-header">
+            <h4 >CONFITRMAR</h4>
+            <br>
+            <h6 >Cambiar Contraseña</h6>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+          </div>
+            <div class="modal-body">
+              <p>Estas seguro de cambiar tu contraseña?</p>
+            </div>
+
+            <div class="modal-footer justify-content-between">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+              <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+    </div>
+    <!-- /.modal-content -->
+  </div>
+  <!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
