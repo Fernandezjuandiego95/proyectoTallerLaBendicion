bb864eb42e2c020407a0ac4b1408ddfcaaa24ac1
moises_mendez <mendezmontalvom@gmail.com>
2023-04-22 14:39:38 -0500

    cambio del head

--- sistema/paginas/plantilla.php
+++ sistema/paginas/plantilla.php
@@ -9,37 +9,6 @@
   <link rel="stylesheet" href="../assets/css/estilos_panel.css">
   <link rel="stylesheet" href="../assets/css/estilos_ajustes.css">
 
-   <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="../assets/plugins/fontawesome-free/css/all.min.css">
-  <link rel="stylesheet" href="../assets/plugins/fontawesome-free/css/solid.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="../assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="../assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- JQVMap -->
-  <link rel="stylesheet" href="../assets/plugins/jqvmap/jqvmap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="../assets/dist/css/adminlte.min.css">
-  <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="../assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="../assets/plugins/daterangepicker/daterangepicker.css">
-  <!-- summernote -->
-  <link rel="stylesheet" href="../assets/plugins/summernote/summernote-bs4.min.css">
-</head>
-
-<body class="hold-transition sidebar-mini layout-fixed" style="background:#F7F8FC;">
-  
-  <div class="wrapper">
-
-    <!-- Preloader -->
-    <div class="preloader flex-column justify-content-center align-items-center">
-      <img class="animation__shake" src="../assets/img/logo_circulo.png" alt="Logo" height="60" width="60">
-    </div>
 
         <?php include "componentes/menu.php";?>
 
